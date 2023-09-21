7bf4ad7e5c84dbbd5b50b1fdc6d5ab81dcd80098
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-21 15:11:26 +0200

    pause

--- content.js
+++ content.js
@@ -1,9 +1,14 @@
 function detecterPartieEchecs() {
     // Analyser le DOM pour détecter une partie d'échecs sur chess.com
     // Vous devrez inspecter la page pour identifier les éléments, classes ou ID spécifiques à une partie d'échecs
-    if (document.querySelector('.specific-class-or-id')) {
+    if (document.querySelector('.board')) {
       return true;
     }
+
+    if(document.getElementById('board-single')) {
+        return true;
+    }
+
     return false;
   }
   
--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 document.getElementById('analyser').addEventListener('click', function() {
+    console.log('Analyser la page')
+    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "detecterPartieEchecs"}, function(response) {
         if (response) {
+            // alert('Une partie d’échecs est détectée sur cette page.')
           console.log('Une partie d’échecs est détectée sur cette page.');
         } else {
+            // alert('Aucune partie d’échecs n’est détectée sur cette page.')
+
           console.log('Aucune partie d’échecs n’est détectée sur cette page.');
         }
       });
