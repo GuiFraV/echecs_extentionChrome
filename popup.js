1428c3117aca94df1a714dbdc6494b960352ff0c
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-21 14:04:32 +0200

    setup +

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.getElementById('analyser').addEventListener('click', function() {
+    // Intégrer une API d'échecs et analyser la position
+    // Afficher le résultat dans le div "resultat"
+
+    console.log('Hello New Extention !')
+
+
+  });
+  
